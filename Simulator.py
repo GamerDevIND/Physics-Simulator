23227e0baebf9e59597533590b0910ab8e1089d9
GamerDevIND <arko2010.mojo@gmail.com>
2025-09-04 21:59:54 +0530

    im dumb

--- Simulator.py
+++ Simulator.py
@@ -32,33 +32,33 @@ while main_loop:
 
     direction_1 = box_1.position - mouse
 
-    screen.blit(font.render(f"Distance: {direction_1.length():.2f}\nSpeed: {box_1.velocity.length():.2f}\nMouse: {mouse.x} / {mouse.y}\nFPS: {int(clock.get_fps())}", True, "white"), (50, 50))
+    # screen.blit(font.render(f"Distance: {direction_1.length():.2f}\nSpeed: {box_1.velocity.length():.2f}\nMouse: {mouse.x} / {mouse.y}\nFPS: {int(clock.get_fps())}", True, "white"), (50, 50))
 
-    if direction_1.length() > 5:
-        box_1.acceleration = direction_1.normalize()  * acceleration_scaler_var
-        acceleration_scaler_var += 0.01
-    else: 
-        box_1.acceleration = pygame.Vector2(0,0)
-        acceleration_scaler_var = acceleration_scaler
+    # if direction_1.length() > 5:
+    #     box_1.acceleration = direction_1.normalize()  * acceleration_scaler_var
+    #     acceleration_scaler_var += 0.01
+    # else: 
+    #     box_1.acceleration = pygame.Vector2(0,0)
+    #     acceleration_scaler_var = acceleration_scaler
 
 
-    box_1.move(damping)
+    # box_1.move(damping)
 
     # if point0.x > screen.get_width() or point0.x < 0:
     #     velocity.x *= -1
     # if point0.y > screen.get_height() or point0.y < 0:
     #     velocity.y *= -1
 
-    if not (0 < box_1.hitbox.centerx < screen.get_width()):
-        box_1.velocity.x *= -1
+    # if not (0 < box_1.hitbox.centerx < screen.get_width()):
+    #     box_1.velocity.x *= -1
     
-    if not (0 < box_1.hitbox.centery < screen.get_height()):
-        box_1.velocity.y *= -1
+    # if not (0 < box_1.hitbox.centery < screen.get_height()):
+    #     box_1.velocity.y *= -1
 
-    if pygame.mouse.get_pressed()[0]:
-        box_1.position = mouse.copy()
-        box_1.velocity = pygame.Vector2(0, 0)
-        box_1.acceleration = pygame.Vector2(0, 0)
+    # if pygame.mouse.get_pressed()[0]:
+    #     box_1.position = mouse.copy()
+    #     box_1.velocity = pygame.Vector2(0, 0)
+    #     box_1.acceleration = pygame.Vector2(0, 0)
 
     box_1.draw(screen)
 
